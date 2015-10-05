4d2bdb9e5efc7083085289a585f6fcd27f92cf00
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-05 01:19:46 -0400

    cambio de algunos detalles
    funciona, pero hay que cambiar nombres de variables para que sea
    consistentes

--- p1.py
+++ p1.py
@@ -7,7 +7,8 @@ Primer intento de implementar runge kutta
 
 m=1.622
 
-f= lambda y,dy: (-y-m*(y**2-1)*dy,dy)
+f= lambda y,dy: ((dy),(-y-m*(y**2-1)*dy))
+
 
 def k1(f,yn,dyn,h):
     fn=f(yn,dyn)
@@ -16,31 +17,31 @@ def k1(f,yn,dyn,h):
     return yk1,dyk1
 
 def k2(f,yn,dyn,h):
-    k_1=k1(yn,dyn,h,f)
-    fn=f(yn+k1[0]/2.,dyn+k_1[1]/2.)
+    k_1=k1(f,yn,dyn,h)
+    fn=f(yn+k_1[0]/2.,dyn+k_1[1]/2.)
     yk2=h*fn[0]
     dyk2=h*fn[1]
     return yk2,dyk2
 
 def k3(f,yn,dyn,h):
-    k_1=k1(yn,dyn,h,f)
-    k_2=k2(yn,dyn,h,f)
+    k_1=k1(f,yn,dyn,h)
+    k_2=k2(f,yn,dyn,h)
     fn=f(yn-k_1[0]+2*k_2[0],dyn-k_1[1]+2*k_2[1])
     yk3=h*fn[0]
     dyk3=h*fn[1]
     return yk3,dyk3
 
 def rk3(f,yn,dyn,h):
-    k_1=k1(yn,dyn,h,f)
-    k_2=k2(yn,dyn,h,f)
-    k_3=k3(yn,dyn,h,f)
-    ym=yn+(1/6)*(k_1[0]+4*k_2[0]+k_3[0])
-    dym=dyn+(1/6)*(k_1[1]+4*k_2[1]+k_3[1])
+    k_1=k1(f,yn,dyn,h)
+    k_2=k2(f,yn,dyn,h)
+    k_3=k3(f,yn,dyn,h)
+    ym=yn+(1./6.)*(k_1[0]+4*k_2[0]+k_3[0])
+    dym=dyn+(1./6.)*(k_1[1]+4*k_2[1]+k_3[1])
     return ym,dym
 
 #Termino de definicion de funciones
 
-f= lambda y,dy: (-y-m*(y**2-1)*dy,dy)
+#f= lambda y,dy: (-y-m*(y**2-1)*dy,dy)
 y0=0.1
 dy0=0
 s0=0
@@ -63,7 +64,13 @@ for t in s:
     dy.append(rk[1])
     i=i+1
     k.append(i)
+s=np.append(s,20*np.pi)
 
+print len(y)
+print len(s)
 plt.figure(1)
 plt.clf()
-plt.plot(s,y)
+plt.plot(y,dy)
+#plt.plot(s,dy)
+plt.draw()
+plt.show()
