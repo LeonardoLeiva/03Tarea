edabdf11d7383f997a588c631f21de2d3e839b71
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-10-06 02:40:52 -0400

    p1 lista
    comentarios y graficos listos

--- p1.py
+++ p1.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 
 '''
 Implementa un método para integrar por Runge Kutta 3 y grafica los resultados de
-una EDO para ciertos valores iniciales.
+una EDO para ciertos valores iniciales. Grafica y(s) vs dy/ds para dichas
+condiciones
 '''
 
-m=1.622
+m=1.622 #RUT=18.668.622-5
 
+#definicion de la funcion a integrar
 f= lambda y,dy: ((dy),(-y-m*(y**2-1)*dy))
 
-
+#pasos para realizar runge kutta
 def k1(f,yn,dyn,h):
     fn=f(yn,dyn)
     yk1=h*fn[0]
@@ -32,6 +34,8 @@ def k3(f,yn,dyn,h):
     dyk3=h*fn[1]
     return yk3,dyk3
 
+#paso de runge kutta
+
 def rk3(f,yn,dyn,h):
     k_1=k1(f,yn,dyn,h)
     k_2=k2(f,yn,dyn,h)
@@ -42,36 +46,70 @@ def rk3(f,yn,dyn,h):
 
 #Termino de definicion de funciones
 
-#f= lambda y,dy: (-y-m*(y**2-1)*dy,dy)
+#condiciones iniciales 1)
 y0=0.1
 dy0=0
-s0=0
-sf=20*np.pi
-n=1000
-h=sf/n
-s=np.linspace(s0,sf,n)
-y=[]
-dy=[]
-y.append(y0)
-dy.append(dy0)
-k=[]
-i=0
-k.append(i)
-for t in s:
-    yn=y[i]
-    dyn=dy[i]
-    rk=rk3(f,yn,dyn,h)
-    y.append(rk[0])
-    dy.append(rk[1])
-    i=i+1
-    k.append(i)
-s=np.append(s,20*np.pi)
 
-print len(y)
-print len(s)
-plt.figure(1)
-plt.clf()
-plt.plot(y,dy)
+s0=0 #valor inicial de la variable independiente
+sf=20*np.pi #valor final de la variable independiente
+n=1000
+h=sf/n #paso
+s=np.linspace(s0,sf,n) #arreglo var indep
+#arreglos para la variable dependiente y su derivada
+y1=[]
+dy1=[]
+y1.append(y0)
+dy1.append(dy0)
+i=0
+for t in s: #iteracion para integrar la funcion
+    yn=y1[i] #saca los valores del instante anterior para calcular el actual
+    dyn=dy1[i]
+    rk=rk3(f,yn,dyn,h)
+    y1.append(rk[0]) #agrega valores obtenidos para y y su derivada
+    dy1.append(rk[1])
+    i=i+1
+s=np.append(s,20*np.pi) #arregla el problema de la dimension de matrices que se
+#presenta. queda pendiente una mejor manera de hacer calzar las dimensiones
+
+#proceso para las segundas condiciones iniciales
+y0=0
+dy0=4
+y2=[]
+dy2=[]
+y2.append(y0)
+dy2.append(dy0)
+i=0
+for t in s:
+    yn=y1[i]
+    dyn=dy1[i]
+    rk=rk3(f,yn,dyn,h)
+    y2.append(rk[0])
+    dy2.append(rk[1])
+    i=i+1
+
+#graficar
+
+fig=plt.figure(1)
+fig.clf()
+ax1=fig.add_subplot(211)
+ax1.plot(y1,dy1)
+plt.title('y(s) vs dy/ds para condiciones iniciales y=0.1 dy/ds=0')
+ax1.set_xlabel('y')
+ax1.set_ylabel('dy/ds')
+ax2=fig.add_subplot(212)
+ax2.plot(y2,dy2)
+plt.title('y(s) vs dy/ds para condiciones iniciales y=0 dy/ds=4')
+ax2.set_xlabel('y')
+ax2.set_ylabel('dy/ds')
+
+
+
+
+#print len(y)
+#print len(s)
+#plt.figure(1)
+#plt.clf()
+#plt.plot(y,dy)
 #plt.plot(s,dy)
 plt.draw()
 plt.show()
